32febd35cbf83de323bd4a1069130da5e5ea0c27
heroliners10 <90538766+heroliners10@users.noreply.github.com>
2022-06-15 20:58:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let password = prompt("Ingrese su Password");
 
 if (password == "12345") {
   alert("Password correcto")
-  window.location.href = "info personal.html";
+  window.location.href = "info-personal.html";
 } else {
   alert("Password incorrecto")
   window.location.href = "denegacion.html";
