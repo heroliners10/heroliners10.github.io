7cba2a6ae629652900a712cc489a6ebb14d7849c
heroliners10 <90538766+heroliners10@users.noreply.github.com>
2022-06-15 21:01:24 -0300

    Add files via upload

---
+++ password.js
@@ -0,0 +1,9 @@
+let password = prompt("Ingrese su Password");
+
+if (password == "12345") {
+  alert("Password correcto")
+  window.location.href = "info-personal.html";
+} else {
+  alert("Password incorrecto.");
+  window.location.href = "denegacion.html";
+}
