5ec7ccb847b29427b14542bc5f2d0f53e00a1d39
heroliners10 <90538766+heroliners10@users.noreply.github.com>
2022-06-15 21:01:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let password = prompt("Ingrese su Password");
-
-if (password == "12345") {
-  alert("Password correcto")
-  window.location.href = "info-personal.html";
-} else {
-  alert("Password incorrecto")
-  window.location.href = "denegacion.html";
-}
